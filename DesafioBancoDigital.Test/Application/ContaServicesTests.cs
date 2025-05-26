0d7d1d9a47805947c2accc7083aa495343a890a8
erick <erick@ERICK>
2025-05-25 22:12:26 -0300

    feat: implementacao do teste de contas

--- DesafioBancoDigital.Test/Application/ContaServicesTests.cs
+++ DesafioBancoDigital.Test/Application/ContaServicesTests.cs
@@ -1,27 +1,24 @@
 ﻿using Application;
 using DesafioBancoDigital.Domain.Entites;
+using DesafioBancoDigital.Domain.Exceptions;
 using DesafioBancoDigital.Domain.Interface;
 using Moq;
+using Xunit;
 
 namespace DesafioBancoDigital.Test.Application
 {
     public class ContaServicesTests
     {
-
         [Fact]
         public async Task SacarTest()
         {
-
             var conta = new Conta(1, 1000);
             var repoMock = new Mock<IContaRepository>();
             repoMock.Setup(r => r.ObterPorNumero(1)).ReturnsAsync(conta);
             repoMock.Setup(r => r.AtualizarSaldo(conta)).Returns(Task.CompletedTask);
 
             var service = new ContaServices(repoMock.Object);
-
-
             var resultado = await service.Sacar(1, 200);
-
 
             Assert.Equal(800, resultado.SaldoConta);
         }
@@ -35,7 +32,6 @@ namespace DesafioBancoDigital.Test.Application
             repoMock.Setup(r => r.AtualizarSaldo(conta)).Returns(Task.CompletedTask);
 
             var service = new ContaServices(repoMock.Object);
-
             var resultado = await service.Depositar(2, 300);
 
             Assert.Equal(800, resultado.SaldoConta);
@@ -49,11 +45,72 @@ namespace DesafioBancoDigital.Test.Application
             repoMock.Setup(r => r.ObterPorNumero(3)).ReturnsAsync(conta);
 
             var service = new ContaServices(repoMock.Object);
-
             var saldo = await service.ObterSaldo(3);
 
             Assert.Equal(1200, saldo);
         }
 
+        [Fact]
+        public async Task SacarContaNaoEncontrada()
+        {
+            var repoMock = new Mock<IContaRepository>();
+            repoMock.Setup(r => r.ObterPorNumero(1)).ReturnsAsync((Conta)null);
+
+            var service = new ContaServices(repoMock.Object);
+            await Assert.ThrowsAsync<ContaNaoEncontradaException>(() => service.Sacar(1, 200));
+        }
+
+        [Fact]
+        public async Task DepositarContaNaoEncontrada()
+        {
+            var repoMock = new Mock<IContaRepository>();
+            repoMock.Setup(r => r.ObterPorNumero(1)).ReturnsAsync((Conta)null);
+
+            var service = new ContaServices(repoMock.Object);
+            await Assert.ThrowsAsync<ContaNaoEncontradaException>(() => service.Depositar(1, 200));
+        }
+
+        [Fact]
+        public async Task ObterSaldoContaNaoEncontrada()
+        {
+            var repoMock = new Mock<IContaRepository>();
+            repoMock.Setup(r => r.ObterPorNumero(1)).ReturnsAsync((Conta)null);
+
+            var service = new ContaServices(repoMock.Object);
+            await Assert.ThrowsAsync<ContaNaoEncontradaException>(() => service.ObterSaldo(1));
+        }
+
+        [Fact]
+        public async Task SacarValorInvalido()
+        {
+            var conta = new Conta(1, 1000);
+            var repoMock = new Mock<IContaRepository>();
+            repoMock.Setup(r => r.ObterPorNumero(1)).ReturnsAsync(conta);
+
+            var service = new ContaServices(repoMock.Object);
+            await Assert.ThrowsAsync<ValorInvalidoException>(() => service.Sacar(1, -100));
+        }
+
+        [Fact]
+        public async Task DepositarValorInvalido()
+        {
+            var conta = new Conta(1, 1000);
+            var repoMock = new Mock<IContaRepository>();
+            repoMock.Setup(r => r.ObterPorNumero(1)).ReturnsAsync(conta);
+
+            var service = new ContaServices(repoMock.Object);
+            await Assert.ThrowsAsync<ValorInvalidoException>(() => service.Depositar(1, -100));
+        }
+
+        [Fact]
+        public async Task SacarSaldoInsuficiente()
+        {
+            var conta = new Conta(1, 1000);
+            var repoMock = new Mock<IContaRepository>();
+            repoMock.Setup(r => r.ObterPorNumero(1)).ReturnsAsync(conta);
+
+            var service = new ContaServices(repoMock.Object);
+            await Assert.ThrowsAsync<SaldoInsuficienteException>(() => service.Sacar(1, 2000));
+        }
     }
 }
